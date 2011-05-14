c2e12a13d6676b8335275fc875827200e0a2bae2
Gavin Williams <gavin@fishrod.co.uk>
2011-05-14 20:01:31 +0100

    Added navigation controller and abandoned creating the navigation from a plist

---
+++ hanginoutwithus/NavigationController.h
@@ -0,0 +1,16 @@
+//
+//  NavigationController.h
+//  hanginoutwithus
+//
+//  Created by Gavin Williams on 08/05/2011.
+//  Copyright 2011 fishrod interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface NavigationController : UIViewController {
+    
+}
+
+@end
